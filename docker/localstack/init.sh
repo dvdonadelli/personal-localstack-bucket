7c9775b1f3ab9742bce9d392a6b9dfb7a8cd2b95
ddonadelli <davi.donadelli@mercadolivre.com>
2023-06-29 14:45:55 -0300

    feat: trying stuff

--- docker/localstack/init.sh
+++ docker/localstack/init.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-set -e
-
-# Wait for LocalStack to be ready
-while ! nc -z localhost 4566; do sleep 1; done
-
-# Create S3 Bucket
-awslocal s3 mb s3://advertising-analytics-test
-
-# Put Objects in Bucket
-awslocal s3 cp src/main/resources/development/dump/relation-query/relation_query_keyword_MLA_2022-12-23.csv s3://advertising-analytics-test/BAds/relation_query_keyword/MLA/
+echo "-------------------------------------Script-01"
+echo "Localstack container executed successfully!"
